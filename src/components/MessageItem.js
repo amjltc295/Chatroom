251b7e0b01fa861e31ca25e8b951837f9d1348cc
Allen <b03901014@ntu.edu.tw>
2018-04-06 16:52:05 +0800

    Add userID on top of messages

--- src/components/MessageItem.js
+++ src/components/MessageItem.js
@@ -3,13 +3,18 @@ import React, { Component, PropTypes } from 'react';
 export default class MessageItem extends Component {
   static propTypes = {
     fromMe: PropTypes.bool.isRequired,
+    userID: PropTypes.string,
     text: PropTypes.string.isRequired,
+    userName: PropTypes.string
   }
 
   render() {
-    const { fromMe, text} = this.props;
+    const { fromMe, userID, text} = this.props;
     return (
       <div className={`message-item ${fromMe ? 'message-from-me' : 'message-from-other'}`}>
+        <div className="message-name">
+          {userID}
+        </div>
         <span>{text}</span>
       </div>
     );
